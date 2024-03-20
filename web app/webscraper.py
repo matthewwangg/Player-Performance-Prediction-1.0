2e0dcbb66318ed476f4af8dc8aa32c5d1357764d
Matt Wang <matthewianwang@gmail.com>
2024-03-20 16:55:27 -0700

    Linked filepaths to players in webscraper

--- web app/webscraper.py
+++ web app/webscraper.py
@@ -3,14 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-# Function to scrape Wikipedia's website which is under Creative Commons Attribution/Share-Alike 4.0 International License
-# https://creativecommons.org/licenses/by-sa/4.0/deed.en
 def scrape_images(player_name):
 
-    player_name = player_name.replace(" ", "_")
-
     # Construct Wikipedia URL
-    url = f"https://en.wikipedia.org/wiki/{player_name}"
+    player_name_formatted = player_name.replace(" ", "_")
+    url = f"https://en.wikipedia.org/wiki/{player_name_formatted}"
 
     # Send a GET request
     response = requests.get(url)
@@ -21,17 +18,30 @@ def scrape_images(player_name):
     soup = BeautifulSoup(response.text, 'html.parser')
     images = soup.find_all('img')
 
-    save_dir = f"static/wikipedia_images/{player_name}"
+    save_dir = f"./static/wikipedia_images/{player_name_formatted}"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+
+    # Initialize a hashtable for the player's images
+    player_images = {}
 
     for img in images:
         img_url = urljoin(url, img['src'])
         img_name = img_url.split("/")[-1]
+        save_path = os.path.join(save_dir, img_name)
 
         # Download and save the image
         img_data = requests.get(img_url).content
-        with open(os.path.join(save_dir, img_name), 'wb') as file:
+        with open(save_path, 'wb') as file:
             file.write(img_data)
 
         print(f"Downloaded {img_name}")
+
+        # Add the save path to the hashtable
+        if player_name not in player_images.keys():
+            player_images[player_name] = [save_path]
+        else:
+            player_images[player_name].append(save_path)
+
+
+    return player_images

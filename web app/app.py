dbe6bd9564190a3f389bf05fc7cb67bd739575af
Matt Wang <matthewianwang@gmail.com>
2024-03-26 20:37:10 -0700

    ***REMOVED***

--- web app/app.py
+++ web app/app.py
@@ -1,21 +1,7 @@
 from flask import Flask, request, render_template
-import pandas as pd
-import xgboost as xgb
-from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import KFold
-import os
-import requests
-import matplotlib
-matplotlib.use('Agg') 
-import matplotlib.pyplot as plt
-from io import StringIO, BytesIO
-import base64
-from xgboost import plot_importance
-from pulp import LpProblem, LpVariable, lpSum, LpMaximize, LpMinimize
 from data_processing import predicts
-import database
+from data_pipeline import initialize_map
+
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -26,6 +12,7 @@ app = Flask(__name__, static_url_path="/static")
 @app.route('/')
 def index():
 
+    initialize_map()
     return render_template('index.html')
     #top_players, optimized_players = predicts()
     #return render_template('index.html', top_players=top_players, optimized_players=optimized_players)
